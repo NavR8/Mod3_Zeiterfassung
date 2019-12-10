53efcfc7ee7d36d81963e66dbb18c57b8004471b
Rooban <rubenworkplace.m3k@gmail.com>
2019-12-10 16:28:15 +0100

    Changes in MaiinActivity.java: created new LOGIN Button and AlertDialog. Created new Fragment layout: dialog_login.xml.
    HomeDialog elements (all buttons and all must me invisible until login is successfull!)
    Same for GalleryFragment (Events, Betriebsfeier).
    
    Reading from Firebase in 2nd layout still crashes program!!!

--- app/src/main/java/com/nt28/modulo3_zeiterfassung/MainActivity.java
+++ app/src/main/java/com/nt28/modulo3_zeiterfassung/MainActivity.java
@@ -1,5 +1,7 @@
 package com.nt28.modulo3_zeiterfassung;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,6 +9,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import android.view.View;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,7 +23,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.util.Calendar;
@@ -91,6 +97,45 @@ public class MainActivity extends AppCompatActivity {
 //                counttime.setText("Finished");
 //            }
 //        };
+
+        Button mShowDialog = findViewById(R.id.showDialogBtn);
+        mShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog_login, null);
+                final EditText mPersNr = mView.findViewById(R.id.etPersonalNr);
+                final EditText mPassword = mView.findViewById(R.id.password);
+                Button mLogin = mView.findViewById(R.id.loginBtn);
+
+                mLogin.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (!mPersNr.getText().toString().isEmpty() && !mPassword.getText().toString().isEmpty()){
+//                           //Original from video on y.tube> Android custom dialog = Create Android Alertdialog with a custom layout.mp4
+                            Toast.makeText(MainActivity.this,
+                                    getString(R.string.login_ok), Toast.LENGTH_SHORT).show();
+
+                            //change background color for Toast
+//                            Toast toast = Toast.makeText(MainActivity.this,  getString(R.string.login_ok), Toast.LENGTH_LONG);
+//                            View view = toast.getView();
+//                            view.setBackgroundResource(R.drawable.custom_background);
+                            //background color change end
+                            //view.getBackground().setColorFilter(getColor., PorterDuff.Mode.SRC_IN);
+//                            view.setBackgroundColor(Color.YELLOW); //ZEIGT keine Aenderung!
+                            //view.getSolidColor(Color.WHITE); //TextColor(Color.WHITE);
+
+                        }else{
+                            Toast.makeText(MainActivity.this,
+                                    getString(R.string.error_login), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                mBuilder.setView(mView);
+                AlertDialog dialog = mBuilder.create();
+                dialog.show();
+            }
+        });
 
 
     }
--- app/src/main/java/com/nt28/modulo3_zeiterfassung/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/nt28/modulo3_zeiterfassung/ui/gallery/GalleryFragment.java
@@ -1,35 +1,102 @@
 package com.nt28.modulo3_zeiterfassung.ui.gallery;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.nt28.modulo3_zeiterfassung.R;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class GalleryFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+     private DatabaseReference refFDB;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//  super.onCreate(savedInstanceState);
+
+//        galleryViewModel =
+//                ViewModelProviders.of(this).get(GalleryViewModel.class);
+        View v = inflater.inflate(R.layout.fragment_gallery, container, false);
+//        final TextView textView = root.findViewById(R.id.text_gallery);
+//        galleryViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
+        refFDB = FirebaseDatabase.getInstance().getReference().child("m3-arbeitszeitenef");
+        refFDB.addValueEventListener(new ValueEventListener() {
+             @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                               final TextView textBFD1 = v.findViewById(R.id.bDatum1);
+
+
+//             final TextView tvEndTime = getView().findViewById(R.id.bDatum1);
+//           String nameBFD1= dataSnapshot.child("m3-arbeitszeitenef").child("Betriebsfeier1").child("Datum").getValue().toString();
+//           tvEndTime.setText(nameBFD1);
+
+                  TextView tvEndTime = getView().findViewById(R.id.bDatum1);
+                 String value = dataSnapshot.child("0").getValue().toString();
+                    Log.d(TAG, value);
+                    tvEndTime.setText((value));
+          }
+
+          @Override
+           public void onCancelled(@NonNull DatabaseError databaseError) {
+              // Failed to read value
+                    Log.w(TAG, "Failed to read value.", databaseError.toException());
+               }
+                });
+
+
+                //Vorschlag 2:
+//        public void basicReadWrite() {
+//            // [START write_message]
+//            // Write a message to the database
+//            FirebaseDatabase database = FirebaseDatabase.getInstance();
+//            DatabaseReference myRef = database.getReference("message");
+//
+//            myRef.setValue("Hello, World!");
+//            // [END write_message]
+//
+//            // [START read_message]
+//            // Read from the database
+//            myRef.addValueEventListener(new ValueEventListener() {
+//                @Override
+//                public void onDataChange(DataSnapshot dataSnapshot) {
+//                    // This method is called once with the initial value and again
+//                    // whenever data at this location is updated.
+//                    String value = dataSnapshot.getValue(String.class);
+//                    Log.d(TAG, "Value is: " + value);
+//                }
+//
+//                @Override
+//                public void onCancelled(DatabaseError error) {
+//                    // Failed to read value
+//                    Log.w(TAG, "Failed to read value.", error.toException());
+//                }
+//            });
+
+        return v;
     }
 }
